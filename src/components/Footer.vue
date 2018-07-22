95405b59c5f559c205e2aa547c378e1758cf6abd
mevlanaayas <mevlanaayas@gmail.com>
2018-07-22 17:42:49 +0300

    addedheader and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+  <footer>
+    <div class="footer-style3">
+      <div class="footer-data3">
+        <div class="container">
+          <div class="footer-logoo">
+            <img src="../../static/images/footer-logo.png" alt="">
+          </div><!--footer-logoo end-->
+          <!--<ul class="footer-links">-->
+          <!--<li><a href="#" title="">Ana Sayfa</a></li>-->
+          <!--<li><a href="#" title="">Projeler</a></li>-->
+          <!--<li><a href="#" title="">Blog</a></li>-->
+          <!--<li><a href="#" title="">Hakkımızda</a></li>-->
+          <!--<li><a href="#" title="">İletişim</a></li>-->
+          <!--</ul>&lt;!&ndash;footer-links end&ndash;&gt;-->
+          <ul class="addrs-info">
+            <li><span>World</span></li>
+            <li><span>info@unichaintr.com</span></li>
+          </ul>
+          <ul class="footer-social-links">
+            <li><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#" title=""><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#" title=""><i class="fa fa-google-plus"></i></a></li>
+            <li><a href="#" title=""><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="#" title=""><i class="fa fa-rss"></i></a></li>
+            <li><a href="#" title=""><i class="fa fa-dribbble"></i></a></li>
+          </ul>
+        </div>
+      </div><!--footer-data3 end-->
+      <div class="bottom-strip2">
+        <div class="container">
+          <div class="cpyright-text">
+            <p>© 2018 UniChain</p>
+          </div><!--cpyright-text end-->
+        </div>
+      </div><!--bottom-strip2 end-->
+    </div>
+  </footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <header class="pb full stick">
+      <div class="header-bar">
+        <div class="logo">
+          <a href="#" title=""><img src="../../static/images/logo.png" alt=""></a>
+        </div><!--logo end-->
+        <div class="mobile-menu-btn">
+          <a href="#" title="" class="open-menu"><i class="fa fa-bars"></i></a>
+        </div><!--mobile-menu-btn end-->
+        <div class="select-language">
+          <ul>
+            <li class="active"><a href="#" title="">TR</a></li>
+            <li><a href="#" title="">ENG</a></li>
+          </ul>
+        </div><!--select-language end-->
+        <nav>
+          <ul>
+            <li><a class="active" href="#" title="">Ana Sayfa</a></li>
+            <li><a href="#whoIam" title="">Hakkımızda</a></li>
+            <li><a href="#projects" title="">Projeler</a></li>
+            <li><a href="#latest" title="">Gelişmeler</a></li>
+            <li><a href="#knock-knock" title="">İletişim</a></li>
+          </ul>
+        </nav><!--nav end-->
+      </div><!--header-bar end-->
+    </header>
+
+    <div class="responsive-mobile-menu">
+      <a href="#" title="" class="close-menu"><i class="ion-close"></i></a>
+      <ul>
+        <li><a class="active" href="#" title="">Ana Sayfa</a></li>
+        <li><a href="#whoIam" title="">Hakkımızda</a></li>
+        <li><a href="#projects" title="">Projeler</a></li>
+        <li><a href="#latest" title="">Gelişmeler</a></li>
+        <li><a href="#knock-knock" title="">İletişim</a></li>
+      </ul>
+    </div><!--responsive-mobile-menu end-->
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
