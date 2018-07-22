1c723d9bf00717b745a83e6801ec0d26a1054512
mevlanaayas <mevlanaayas@gmail.com>
2018-07-22 17:43:17 +0300

    arrnaged template page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="page-loading">
+      <div class="thecube">
+        <div class="cube c1"></div>
+        <div class="cube c2"></div>
+        <div class="cube c4"></div>
+        <div class="cube c3"></div>
+      </div>
+    </div><!--page-loading end-->
+    <div class="wrapper">
+      <app-header></app-header>
+      <router-view/>
+      <app-footer></app-footer>
+    </div><!--wrapper end-->
   </div>
 </template>
 
 <script>
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    appFooter: Footer,
+    appHeader: Header,
+  }
 }
 </script>
 
