311c088fae5c865e6668c7f5bda428923149f189
Fatih Altuntas <altuntasfatih42@gmail.com>
2018-08-15 00:17:16 +0300

    Fix cross origin

--- server/server.js
+++ server/server.js
@@ -13,7 +13,9 @@ app.use( bodyParser.json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
