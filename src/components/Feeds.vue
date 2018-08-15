3460d6c5aa2c8dd35f81d394766934b895bce01c
Fatih Altuntas <altuntasfatih42@gmail.com>
2018-08-15 15:58:46 +0300

    Update

--- src/components/Feeds.vue
+++ src/components/Feeds.vue
@@ -2,19 +2,21 @@
   <div class="block no-padding pd-rs">
     <div class="posts-sec st3 bd-df">
       <div class="row">
-        <div class="col-lg-3 col-md-4 col-sm-6"  v-for="post in posts">
+        <div class="col-lg-3 col-md-4 col-sm-6 align-middle "  v-for="post in posts">
+          <a  :href="post.link[0]" target="_blank">
           <div class="post">
             <div class="post-img">
-              <img src="http://via.placeholder.com/480x432" alt="">
+              <img  src="https://cdn-images-1.medium.com/max/720/1*RkrXVq-its7PCq3xYoHvEg.jpeg" alt="">
             </div>
             <div class="post-details">
               <ul class="post-info">
-                <li><span>Tarih</span></li>
-                <li><a href="#" title="">Onur Amcanız</a></li>
+                <li><span>{{getDate(post.pubDate[0])}}</span></li>
+                <li><a>{{post.creator[0].toUpperCase()}}</a></li>
               </ul>
-              <h3><a href="#" title="">{{post.title[0]}}</a></h3>
-            </div>
+              <h3><a>{{post.title[0]}}</a></h3></div>
+
           </div><!--post end-->
+          </a>
         </div>
 
 
@@ -44,9 +46,24 @@ export default {
 
     })
   },
+  methods:{
+    getDate:function (date) {
+      var mydate = new Date(date);
+      return mydate.toDateString()
+    },
+    getImage:function (des) {
+      let tagIndex = des.indexOf('<img'); // Find where the img tag starts
+      let srcIndex = des.substring(tagIndex).indexOf('src=') + tagIndex; // Find where the src attribute starts
+      let srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
+      let srcEnd = des.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
+      let src = des.substring(srcStart, srcEnd); // Extract just the URL
+      console.log(src)
+      return src
+
+    }
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
