06874e7a412ce16d743b3053201d3feba0afb4e8
Fatih Altuntas <altuntasfatih42@gmail.com>
2018-08-17 11:50:47 +0300

    Added bootstrap vue

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import web3 from 'web3'
 import App from './App'
 import router from './router'
 import store from './store'
+import BootstrapVue from "bootstrap-vue"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+Vue.use(BootstrapVue)
+
 
 
 Vue.config.productionTip = false
